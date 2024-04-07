6066fa01e9c0636b111c06dc246ace1abce53ae9
Lee Xenon <478803244@qq.COM>
2024-04-07 15:07:18 +0800

    Update LXANCS.h

--- LXANCS.h
+++ LXANCS.h
@@ -1,14 +1,7 @@
 #ifndef LXANCS_H
 #define LXANCS_H
 
-//#include "messagePage.h"
-
-extern "C"{
-    #include "trans_pwm.h"
-    #include "messagePage.h"
-}
-
-
 extern "C" void lx_ancs_begin();
 extern "C" void lx_ancs_end();
+
 #endif
